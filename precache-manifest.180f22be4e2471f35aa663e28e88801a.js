e25e0aa2de3625503e7eb223cc90d78777e9f89c
regisbr13 <regislima1391@gmail.com>
2020-05-27 16:56:51 -0300

    Updates

---
+++ precache-manifest.180f22be4e2471f35aa663e28e88801a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44153c123c83bd4bfb8cf2e952ff07f7",
+    "url": "/instagram-react-clone/index.html"
+  },
+  {
+    "revision": "97212ff5bd13384e8036",
+    "url": "/instagram-react-clone/static/css/main.881a6c83.chunk.css"
+  },
+  {
+    "revision": "02b8d0b1b7ed434aa552",
+    "url": "/instagram-react-clone/static/js/2.d4cc3669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/instagram-react-clone/static/js/2.d4cc3669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97212ff5bd13384e8036",
+    "url": "/instagram-react-clone/static/js/main.fcd70a74.chunk.js"
+  },
+  {
+    "revision": "958f9b8aecd91d6a7dbb",
+    "url": "/instagram-react-clone/static/js/runtime-main.ca79537f.js"
+  },
+  {
+    "revision": "1ff53bddbdfc9180c33167ee29befa58",
+    "url": "/instagram-react-clone/static/media/instagram-logo.1ff53bdd.svg"
+  }
+]);
